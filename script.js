526769541702bb5038217bf18f5af0239254b0f9
djenkins96 <derrick.jenkins@me.com>
2017-06-13 18:10:01 -0400

    finished lab

--- script.js
+++ script.js
@@ -1,62 +1,107 @@
 //shape, circle, triangle, rectangle, square
+var rectangleHeight = document.getElementById('rectangle-height');
+var rectangleWidth = document.getElementById('rectangle-width');
+var circleVal = document.getElementById('circle-form');
+var triangleVal = document.getElementById('triangle-form');
+var squareVal = document.getElementById('square-form');
 
-function Shape(name, height, width) {
-    this.x = Math.random() * 500;
-    this.y = Math.random() * 500;
+var shapeNameLabel = document.getElementById('shape-name');
+var shapeWidthLabel = document.getElementById('shape-width');
+var shapeHeightLabel = document.getElementById('shape-height');
+var shapeRadiusLabel = document.getElementById('shape-radius');
+var shapeAreaLabel = document.getElementById('shape-area');
+var shapePerimeterLabel = document.getElementById('shape-perimeter');
+
+
+//event listeners
+
+document.getElementById('circle-button').addEventListener('click', function () {
+    new Circle(circleVal.value);
+});
+document.getElementById('rectangle-button').addEventListener('click', function () {
+    new Rectangle(rectangleHeight.value, rectangleWidth.value);
+});
+document.getElementById('triangle-button').addEventListener('click', function () {
+    new Triangle(triangleVal.value, triangleVal.value);
+});
+document.getElementById('square-button').addEventListener('click', function(){
+    new Square (squareVal.value);
+});
+
+
+function Shape(height, width) {
     this.height = height;
     this.width = width;
+}
+    
+
+Shape.prototype.draw = function(){
     this.div = document.createElement('div');
-    this.div.className = 'shape';
+    this.div.className = 'shape ' + this.cssClass;
+    
+    this.x = Math.floor(Math.random() * (600 - this.height));
+    this.y = Math.floor(Math.random() * (600 - this.width));
+
     this.div.style.top = this.y + 'px';
     this.div.style.left = this.x + 'px';
+    this.div.style.width = this.width + 'px';
+    this.div.style.height = this.height + 'px';
+
+     this.div.addEventListener('click', this.describe.bind(this));
+    this.div.addEventListener('dblclick', function() {
+        this.removeDiv();
+    }.bind(this));
     document.getElementById('sand-box').appendChild(this.div);
-    this.div.addEventListener('dblclick', function () {
-        this.divClick();
-    }.bind(this));
-    this.div.addEventListener('click', function () {
-        console.log('you clicked this div!')
-    }.bind(this));
 }
 
-Shape.prototype.divClick = function () {
+Shape.prototype.describe = function() {
+    shapeNameLabel.innerText = this.constructor.name;
+    shapeWidthLabel.innerText = this.width;
+    shapeHeightLabel.innerText = this.height;
+    shapeRadiusLabel.innerText = this.radius;
+    shapeAreaLabel.innerText = this.area();
+    shapePerimeterLabel.innerText = this.perimeter();
+}
+
+Shape.prototype.removeDiv = function () {
     this.div.remove();
 }
 
-function Rectangle(height, width) {
-    Shape.call(this, 'Rectangle', height, width);
-    this.height = height;
-    this.width = width;
-    this.div.height = rectangleHeight.value + 'px';
-    this.div.width = rectangleWidth.value + 'px';
-    this.div.className = 'rectangle';
+
+
+
+
+function Rectangle(width, height) {
+    Shape.call(this, width, height);
+    this.cssClass = 'rectangle';
+    this.draw();
 }
 
-// rectangle button
-var rectangleHeight = document.getElementById('rectangle-height');
-var rectangleWidth = document.getElementById('rectangle-width');
 
-document.getElementById('rectangle-button').addEventListener('click', function () {
-    new Rectangle(rectangleHeight.value, rectangleWidth.value);
-    
-})
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
 
-var circleVal = document.getElementById('circle-form');
+Rectangle.prototype.area = function() {
+    return this.width * this.height;
+}
+
+Rectangle.prototype.perimeter = function() {
+    return 2 * this.width + 2 * this.height;
+}
+
+
 
 function Circle(radius) {
-    Shape.call(this, 'Circle', radius, null);
+    Shape.call(this, 2 * radius, 2 * radius);
     this.radius = radius;
-    this.div.style.width = this.diameter() + 'px';
-    this.div.style.height = this.diameter() + 'px';
-    this.div.className = 'circle';
-
+    this.cssClass = 'circle'
+    this.draw();
 }
 
 Circle.prototype = Object.create(Shape.prototype);
 Circle.prototype.constructor = Circle;
 //circle button
-document.getElementById('circle-button').addEventListener('click', function () {
-    new Circle(circleVal.value);
-})
+
 
 
 Circle.prototype.area = function () {
@@ -71,27 +116,38 @@ Circle.prototype.diameter = function () {
 
 
 
-var Triangle = function (height, width) {
-    Shape.call(this, 'Triangle', height, width);
-    this.height = 0;
-    this.width = 0;
-    this.div.className = 'triangle';
-    this.div.style.height = triangleVal.value + 'px';
-    this.div.style.width = triangleVal.value + 'px';
+function Triangle (height) {
+    Shape.call(this, height, height);
+    this.cssClass = 'triangle';
+    this.draw();
+    this.div.style.width = '0';
+    this.div.style.height = '0';
+    this.div.style.borderRightWidth = height + 'px';
+    this.div.style.borderBottomWidth = height + 'px';
 }
 
 Triangle.prototype = Object.create(Shape.prototype);
 Triangle.prototype.constructor = Triangle;
 
-//trianglebutton
-document.getElementById('triangle-button').addEventListener('click', function () {
-    new Triangle(triangleVal.value, triangleVal.value);
-});
+Triangle.prototype.area = function() {
+    return 0.5 * this.height * this.height;
+}
+Triangle.prototype.perimeter = function() {
+    return 2 * this.height + Math.sqrt(2 * Math.pow(this.height, 2));
+}
 
-var triangleVal = document.getElementById('triangle-form');
 
 
 
+
+function Square(side) {
+    Rectangle.call(this, side, side);
+    this.cssClass = 'square';
+    this.div.classList.remove('rectangle');
+    this.div.classList.add(this.cssClass);
+}
+Square.prototype = Object.create(Rectangle.prototype);
+Square.prototype.constructor = Square;
 
 
 
